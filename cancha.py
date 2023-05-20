ef1ae6204c87b2b1ca6b9d092c3a6ad79e19b592
m4idy <maidy2234507@correo.uis.edu.co>
2023-05-20 08:15:31 -0500

    codigo

--- cancha.py
+++ cancha.py
@@ -13,7 +13,6 @@ canvas.create_rectangle(50, 50, 950, 550, outline="white", width=2)
 canvas.create_line(500, 550, 500, 50, fill="white", width=2)
 canvas.create_rectangle(800, 180, 950, 440, outline="white", width=2)
 canvas.create_arc(375, 200, 625, 450, start=0, extent=359, style="arc", outline="white", width=2)
-canvas.create_arc(750, 230, 850, 390, start=90, extent=180, style="arc", outline="white", width=2)
 canvas.create_rectangle(50, 220, 130, 400, outline="white", width=2)
 canvas.create_rectangle(870, 220, 950, 400, outline="white", width=2)
 
